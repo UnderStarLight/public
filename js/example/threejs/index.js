ba4678f87b3de277d076e9f817004f9da24b45ed
UnderStarLight <qinhuyi@outlook.com>
2023-05-26 23:05:55 +0800

    threejs

---
+++ js/example/threejs/index.js
@@ -0,0 +1,5 @@
+// inport threejs
+import * as THREE from 'three';
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
+import Stats from 'three/examples/jsm/libs/stats.module.js';
